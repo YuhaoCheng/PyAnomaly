9eb9e0ee1d448e33d095a6d97982a5d4e437ae9e
YuhaoCheng <446358161@qq.com>
2021-02-01 16:12:40 +0800

    add the setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+
+from setuptools import setup
+
+setup(
+    name='pyanomaly',
+    version='v0.1',
+    description='This is the package used for anomaly detection in video',
+    author='YuhaoCheng',
+    url='https://github.com/YuhaoCheng/PyAnomaly',
+    packages=['pyanomaly']
+)
